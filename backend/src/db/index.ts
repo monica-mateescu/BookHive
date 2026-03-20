9df5c24a661a98f7444bbfd201db377bd0c8f0c4
Monica Mateescu <monica.antonie@gmail.com>
2026-03-20 10:53:41 +0100

    fix: correct MongoDb URI variable name

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import { MONGODB_URI, DB_NAME } from "#config";
+import mongoose from 'mongoose';
+import { MONGO_URI, DB_NAME } from '#config';
 
 try {
-  const clinet = await mongoose.connect(MONGODB_URI, {
-    dbName: DB_NAME,
+  const clinet = await mongoose.connect(MONGO_URI, {
+    dbName: DB_NAME
   });
   console.log(`Connected to MongoDB: ${clinet.connection.name}`);
 } catch (error) {
-  console.error("Error connecting to MongoDB:", error);
+  console.error('Error connecting to MongoDB:', error);
   process.exit(1);
 }
