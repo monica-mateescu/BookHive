788039975f0b8ea3f5c424444281c33c60d19902
Monica Mateescu <monica.antonie@gmail.com>
2026-04-02 11:13:41 +0200

    feat: add index file for page components

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
+export { default as NotFound } from "./NotFound";
+export { default as Dashboard } from "./dashboard/Dashboard";
+export { default as CreateBook } from "./dashboard/Book";
+export { default as Books } from "./dashboard/Books";
