e424abd5998e92c01f8bfdd45f463104d2ccd569
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-04-02 15:26:17 +0200

    fix: fix dashboard components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as BookForm } from "./dashboard/BookForm";
-export { default as BookCard } from "./dashboard/BookCard";
 export { default as ClubForm } from "./dashboard/ClubForm";
 export { default as BookRow } from "./dashboard/BookRow";
 export { default as SidebarNav } from "./dashboard/SidebarNav";
