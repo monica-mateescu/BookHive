0bae0e4dac6f50b60b885ef9f3792bed0a12a8c1
Monica Mateescu <monica.antonie@gmail.com>
2026-04-08 10:48:17 +0200

    feat: add ClubRow component

---
+++ frontend/src/components/dashboard/ClubRow.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router";
+
+import type { Club } from "../../types/club";
+
+type ClubRowProps = {
+  index: number;
+  club: Club;
+  onDelete: () => void;
+};
+
+const ClubRow = ({ index, club, onDelete }: ClubRowProps) => {
+  return (
+    <tr>
+      <th>{index}</th>
+      <td>{club.name}</td>
+      <td>{club.description}</td>
+      <td>{club.isActive ? "Yes" : "No"}</td>
+      <td>{new Date(club.createdAt).toLocaleDateString()}</td>
+      <th>
+        <div className="flex justify-end gap-3">
+          <Link to={`${club.id}`} className="btn btn-info btn-xs">
+            view
+          </Link>
+          <Link to={`${club.id}/edit`} className="btn btn-warning btn-xs">
+            edit
+          </Link>
+          <button onClick={onDelete} className="btn btn-error btn-xs">
+            delete
+          </button>
+        </div>
+      </th>
+    </tr>
+  );
+};
+
+export default ClubRow;
