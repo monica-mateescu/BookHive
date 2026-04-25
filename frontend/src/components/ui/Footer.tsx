76b22b755e0e2ba870acb2093f014b8cbc799389
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-04-25 17:14:09 +0200

    feat: footer styled

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router";
+
 function Footer() {
   return (
-    <footer className="flex items-center justify-center bg-gray-400 p-5 text-black md:flex-row">
-      Footer
+    <footer className="flex items-center justify-center border-t-2 border-t-(--brand-primary) p-5 text-(--brand-secondary) md:flex-row">
+      <Link
+        to="/about"
+        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+      >
+        About
+      </Link>
+      <Link
+        to="/privacy-policy"
+        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+      >
+        Privacy Policy
+      </Link>
+      <Link
+        to="/contact"
+        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+      >
+        Contact
+      </Link>
     </footer>
   );
 }
