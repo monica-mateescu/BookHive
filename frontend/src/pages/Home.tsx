93cd67f1911f0f16296a0297db868d0a8f7f3381
Monica Mateescu <monica.antonie@gmail.com>
2026-04-28 15:06:29 +0200

    feat: update Home component layout with welcoming message and book exploration prompt

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,33 @@
 import { Clubs } from "@/components";
-import { Books } from "@/components";
+import { Link } from "react-router";
 
 function Home() {
   return (
     <>
-      <h1 className="text-center">Home</h1>
-      <Clubs />
-      <br/>
-      <Books />
+      <section className="mb-10 text-center">
+        <h1 className="text-3xl font-semibold">
+          Welcome to our vibrant book club community!
+          <span className="block text-lg text-(--gray-primary)">
+            Join a club to connect with fellow book lovers, share your thoughts,
+            and embark on exciting literary adventures together.
+          </span>
+        </h1>
+        <p className="text-md text-(--gray-primary)">
+          Not sure where to start? Explore our{" "}
+          <Link to="/books" className="text-(--brand-primary) underline">
+            collection of books
+          </Link>{" "}
+          and find the perfect read to spark your next club discussion.
+        </p>
+      </section>
+
+      <section aria-labelledby="clubs-title">
+        <h2 id="clubs-title" className="sr-only">
+          Book clubs list
+        </h2>
+
+        <Clubs />
+      </section>
     </>
   );
 }
