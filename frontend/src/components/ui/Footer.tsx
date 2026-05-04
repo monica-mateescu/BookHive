076f279438c5e10de208a95bb78debcc57cf5aa3
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-05-04 18:09:16 +0200

    feat: add rights to footer

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -2,25 +2,28 @@ import { Link } from "react-router";
 
 function Footer() {
   return (
-    <footer className="flex items-center justify-center border-t-2 border-t-(--brand-primary) p-5 text-(--brand-secondary) md:flex-row">
-      <Link
-        to="/about"
-        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
-      >
-        About
-      </Link>
-      <Link
-        to="/privacy-policy"
-        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
-      >
-        Privacy Policy
-      </Link>
-      <Link
-        to="/contact"
-        className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
-      >
-        Contact
-      </Link>
+    <footer className="flex flex-col border-t-2 border-t-(--brand-primary) p-5 text-center text-(--brand-secondary)">
+      <div className="mb-2 flex flex-col items-center justify-center md:flex-row">
+        <Link
+          to="/about"
+          className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+        >
+          About
+        </Link>
+        <Link
+          to="/privacy-policy"
+          className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+        >
+          Privacy Policy
+        </Link>
+        <Link
+          to="/contact"
+          className="px-5 text-lg no-underline hover:text-(--brand-primary) hover:underline"
+        >
+          Contact
+        </Link>
+      </div>
+      © 2026 All rights reserved
     </footer>
   );
 }
