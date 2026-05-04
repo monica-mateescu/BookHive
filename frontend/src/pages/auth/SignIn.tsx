8ab1560781a92a717d5b2ba2e4e6dc25fdb68be0
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-05-04 15:52:03 +0200

    feat: sign-in form daisy-ui styled

--- frontend/src/pages/auth/SignIn.tsx
+++ frontend/src/pages/auth/SignIn.tsx
@@ -3,7 +3,10 @@ import { SignInForm } from "@/components";
 function SignIn() {
   return (
     <>
-      <h1 className="text-center">Sign in</h1>
+      <h1 className="text-center text-3xl font-semibold">Sign in</h1>
+      <div className="mt-2 text-center text-(--gray-primary)">
+        Sign in to get started
+      </div>
       <SignInForm />
     </>
   );
