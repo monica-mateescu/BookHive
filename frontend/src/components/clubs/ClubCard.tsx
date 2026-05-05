18d021c66fb3d4642915c32004b34f59bf36ea09
Monica Mateescu <monica.antonie@gmail.com>
2026-05-05 16:12:16 +0200

    fix: update layout of ClubCard component for improved structure and spacing

--- frontend/src/components/clubs/ClubCard.tsx
+++ frontend/src/components/clubs/ClubCard.tsx
@@ -19,34 +19,35 @@ function ClubCard({ club }: ClubCardProps) {
 
   return (
     <Link to={`/clubs/${club.id}`}>
-      <div className="flex flex-col rounded-lg bg-(--bg-main)/80 p-5 shadow-lg ring-1 ring-black/5 backdrop-blur-sm transition hover:shadow-xl">
-        <div className="mb-2 h-44">
+      <div className="flex flex-col rounded-lg bg-(--bg-main)/80 shadow-lg ring-1 ring-black/5 backdrop-blur-sm transition hover:shadow-xl">
+        <div className="h-44 overflow-hidden rounded-tl-lg rounded-tr-lg">
           <img
             src={club.image ?? clubImage}
             alt={club.name}
             className="h-full w-full object-cover"
           />
         </div>
+        <div className="p-5">
+          <h2 className="mb-1 text-sm font-semibold">{club.name}</h2>
 
-        <h2 className="mb-1 text-sm font-semibold">{club.name}</h2>
-
-        <div className="flex items-stretch gap-3 text-(--brand-secondary)">
-          <div className="h-24 shrink-0">
-            <img
-              src={bookImage}
-              alt={bookTitle}
-              className="h-full w-auto object-cover"
-            />
-          </div>
-
-          <div className="flex h-24 flex-1 flex-col">
-            <div className="flex flex-1 flex-col justify-center text-xs">
-              <h3 className="font-medium">{bookTitle}</h3>
-              <span>Members: {club.members.length}</span>
+          <div className="flex items-stretch gap-3 text-(--brand-secondary)">
+            <div className="h-24 shrink-0">
+              <img
+                src={bookImage}
+                alt={bookTitle}
+                className="h-full w-auto object-cover"
+              />
             </div>
 
-            <div className="flex justify-end">
-              <Button> More details</Button>
+            <div className="flex h-24 flex-1 flex-col">
+              <div className="flex flex-1 flex-col justify-center text-xs">
+                <h3 className="font-medium">{bookTitle}</h3>
+                <span>Members: {club.members.length}</span>
+              </div>
+
+              <div className="flex justify-end">
+                <Button> More details</Button>
+              </div>
             </div>
           </div>
         </div>
