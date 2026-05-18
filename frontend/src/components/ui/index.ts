e02d77e8045635e81e02fe0467fe5717463810a0
Monica Mateescu <monica.antonie@gmail.com>
2026-05-18 16:24:14 +0200

    feat: add ErrorAlert and SuccessAlert components

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,3 +1,5 @@
 export { default as InfoState } from "./alert/InfoState";
 export { default as ErrorState } from "./alert/ErrorState";
+export { default as SuccessAlert } from "./alert/SuccessAlert";
+export { default as ErrorAlert } from "./alert/ErrorAlert";
 export { default as Button } from "./Button";
