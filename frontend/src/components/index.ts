aa5a02db366f712a0937438737e87e888676d36e
Monica Mateescu <monica.antonie@gmail.com>
2026-06-09 22:37:13 +0200

    feat(clubs): add PopularClubs component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -7,6 +7,7 @@ export {
 export { Hero } from "./ui";
 export { default as Clubs } from "./clubs/Clubs";
 export { default as UpcomingClubs } from "./clubs/UpcomingClubs";
+export { default as PopularClubs } from "./clubs/PopularClubs";
 export { default as ClubCard } from "./clubs/ClubCard";
 export { default as MyClubCard } from "./clubs/MyClubCard";
 export { default as ClubDetail } from "./clubs/ClubDetail";
