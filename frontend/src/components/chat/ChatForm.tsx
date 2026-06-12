331716f7e3126ba666a40b96dbdd273ff2d8d4ce
Anna Tsyryatyeva <anny-b@yandex.ru>
2026-06-12 11:11:50 +0200

    fix: fix chat position

--- frontend/src/components/chat/ChatForm.tsx
+++ frontend/src/components/chat/ChatForm.tsx
@@ -47,63 +47,64 @@ function ChatForm({ chatId, isConnected }: Chat) {
       : "absolute right-5 inset-y-0 my-auto h-fit text-(--brand-primary) hover:text-(--brand-secondary) focus:text-(--brand-secondary) cursor-pointer transition-colors";
 
   return (
-    <div className="mt-auto w-full">
-      <form onSubmit={onSubmit} className="mt-5 w-full">
-        {error && (
-          <div role="alert" className="alert alert-error mb-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>{error}</span>
-          </div>
-        )}
-
-        <div className="relative w-full">
-          <div>
-            <label htmlFor="message" className="sr-only">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="textarea textarea-bordered max-h-24 min-h-24 w-full resize-none"
-              placeholder="Write a message..."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={handleKeyDown}
-              required
-              disabled={submitting}
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={!canSubmit || submitting}
-            className={buttonClass}
-            aria-label="Send message"
+    <form
+      onSubmit={onSubmit}
+      className="mt-2 w-full rounded shadow-[0_2px_5px_rgba(0,0,0,0.5)]"
+    >
+      {error && (
+        <div role="alert" className="alert alert-error mb-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
           >
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-              <path
-                d="M1 8H15M15 8L8 1M15 8L8 15"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}</span>
         </div>
-      </form>
-    </div>
+      )}
+
+      <div className="relative">
+        <div>
+          <label htmlFor="message" className="sr-only">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="textarea textarea-bordered max-h-24 min-h-24 w-full resize-none"
+            placeholder="Write a message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+            required
+            disabled={submitting}
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={!canSubmit || submitting}
+          className={buttonClass}
+          aria-label="Send message"
+        >
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path
+              d="M1 8H15M15 8L8 1M15 8L8 15"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </form>
   );
 }
 
