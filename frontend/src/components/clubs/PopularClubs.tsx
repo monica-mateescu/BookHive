bf0b43e7b9430a63abf5f8ba5b334bbf4608cef3
Monica Mateescu <monica.antonie@gmail.com>
2026-06-17 16:07:09 +0200

    refactor: simplify club display logic in PopularClubs and UpcomingClubs components

--- frontend/src/components/clubs/PopularClubs.tsx
+++ frontend/src/components/clubs/PopularClubs.tsx
@@ -2,7 +2,7 @@ import { getPopularClubs } from "@data";
 import { useQuery } from "@tanstack/react-query";
 import type { Club } from "@types";
 
-import { ClubCard, ClubListSkeleton, EmptyState, ErrorState } from "..";
+import { ClubCard, ClubListSkeleton, ErrorState } from "..";
 
 const PopularClubs = () => {
   const { data, isLoading, isError } = useQuery<Club[]>({
@@ -18,29 +18,24 @@ const PopularClubs = () => {
     return (
       <ErrorState message="Something went wrong, we couldn’t load the data. Please try again later." />
     );
-
   return (
-    <section aria-labelledby="popular-clubs-title">
-      <div className="mb-6 flex items-center justify-between">
-        <h2
-          id="popular-clubs-title"
-          className="text-xl font-semibold text-(--text-main)"
-        >
-          Popular clubs
-        </h2>
-      </div>
-      {clubs.length === 0 ? (
-        <EmptyState message="No popular clubs found." />
-      ) : (
-        <>
-          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {clubs.map((club) => (
-              <ClubCard key={club.id} club={club} variant="popular" />
-            ))}
-          </div>
-        </>
-      )}
-    </section>
+    clubs.length > 0 && (
+      <section aria-labelledby="popular-clubs-title">
+        <div className="mb-6 flex items-center justify-between">
+          <h2
+            id="popular-clubs-title"
+            className="text-xl font-semibold text-(--text-main)"
+          >
+            Popular clubs
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {clubs.map((club) => (
+            <ClubCard key={club.id} club={club} variant="popular" />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
