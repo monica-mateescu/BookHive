3dd889b1cf64baec6326ce31dc474f649ccca6f9
Monica Mateescu <monica.antonie@gmail.com>
2026-06-23 12:54:40 +0200

    refactor: simplify PopularClubs and UpcomingClubs components by removing error handling and restructuring layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,17 +9,8 @@ function Home() {
           <SearchClub />
         </div>
       </div>
-      <div className="section">
-        <div className="container">
-          <UpcomingClubs />
-        </div>
-      </div>
-
-      <div className="section section--muted">
-        <div className="container">
-          <PopularClubs />
-        </div>
-      </div>
+      <UpcomingClubs />
+      <PopularClubs />
     </>
   );
 }
