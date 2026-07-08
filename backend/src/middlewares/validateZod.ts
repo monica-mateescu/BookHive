ef7c1e84c42519840d3d0978f74a55a747a7fbfc
Monica Mateescu <monica.antonie@gmail.com>
2026-07-08 14:42:11 +0200

    refactor: split validateZod into validateBody and validateQuery for clarity

--- backend/src/middlewares/validateZod.ts
+++ backend/src/middlewares/validateZod.ts
@@ -1,15 +1,30 @@
 import type { RequestHandler } from 'express';
 import { z, type ZodObject } from 'zod';
 
-export const validateZod =
-  (zodSchema: ZodObject): RequestHandler =>
+export const validateQuery =
+  (zodSchema: ZodObject<any>): RequestHandler =>
   (req, _res, next) => {
-    const { data, error, success } = zodSchema.safeParse({ ...req.body, ...req.query });
+    const { data, error, success } = zodSchema.safeParse(req.query);
+
     if (!success) {
       next(new Error(z.prettifyError(error), { cause: { status: 400 } }));
       return;
     }
-    if (req.body) req.body = data;
 
+    Object.assign(req.query, data);
+    next();
+  };
+
+export const validateBody =
+  (zodSchema: ZodObject<any>): RequestHandler =>
+  (req, _res, next) => {
+    const { data, error, success } = zodSchema.safeParse(req.body);
+
+    if (!success) {
+      next(new Error(z.prettifyError(error), { cause: { status: 400 } }));
+      return;
+    }
+
+    req.body = data;
     next();
   };
